1649bf2cdf210fcbe58d588ff9c44fca3b646c1e
Hafize NALBANT <hafize.nalbant@turmob.org.tr>
2026-01-13 11:36:48 +0300

    test commit

--- WebAPI/Controllers/Scorm12Controller.cs
+++ WebAPI/Controllers/Scorm12Controller.cs
@@ -18,6 +18,7 @@ namespace WebAPI.Controllers
         [HttpPost("commit/{attemptId:guid}")]
         public async Task<IActionResult> Commit(Guid attemptId, [FromBody] Dictionary<string, string> data)
         {
+            var a = 12;
             var result = await _runtime.HandleCommitAsync(attemptId, data);
             if (!result.Success)
                 return BadRequest(result.Message);
